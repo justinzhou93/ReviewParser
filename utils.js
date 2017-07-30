4f7ee9d212ef521a393a704e230c75e597fe96f5
Justin Zhou <justinzhou93@gmail.com>
2017-07-30 12:37:38 -0400

    test(basic console logs): not working. choking on nextpage problem

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 module.exports = {
     typeCheck: function(input, nightmare){
+        console.log(`in typecheck, input = ${input}`);
         let DOMlegend = {
             id: '#',
             class: '.',
@@ -13,6 +14,7 @@ module.exports = {
         }
     },
     contentTypeCheck: function(review, contentTag){
+        console.log(`in contenttypecheck, arguments = ${arguments}`);
         if (typeof input === Object){
             if (contentTag.identifier === 'element'){
                 return review.getElementsByTagName(contentTag.element)[0];
@@ -25,6 +27,7 @@ module.exports = {
     },
     reviewParse: function(review, data, goodbad){
         // add words to data one by one
+        console.log(`in reviewParse, arguments = ${arguments}`);
         for (let i = 0; i < review.length; i++){
             // NOTE: remove stopwords later, when you're looking for the top words. should save computation
             if (data.hasOwnProperty(review[i])){
@@ -36,6 +39,7 @@ module.exports = {
     },
     // FIXME: need to finish how to check for good or bad review
     starCount: function(rating, startext){
+        console.log(`in starCount, arguments = ${arguments}`);
         let parsedRating;
         // test = x || y
         if (startext === 'title'){
